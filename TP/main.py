6c9a7e7d91f588ab989676cc27ddf0a450ae0314
Juanseaven <123155034+Juanseaven@users.noreply.github.com>
2024-11-08 18:10:49 -0300

    Update main.py

--- TP/main.py
+++ TP/main.py
@@ -5,18 +5,29 @@ pos_angular_asteroide = 3.025
 
 def aceleracion_asteroide(r):
   G = 6.667430e-11
-  masa_asteroide = M
+  M = masa_asteroide
   return -G*M/r**2
 
-def adam_bashforth_3(y,f,delta_t):
-  return y[-1] + (delta_t / 12) * (23 * f[-1] - 16 * f[-2] + 5 * f[-3])
+def adam_bashforth_3(y_valores,f_valores,delta_t):
+  return y_valores[-1] + (delta_t / 12) * (23 * f_valores[-1] - 16 * f_valores[-2] + 5 * f_valores[-3])
 
 def f(v0):
   r = pos_radial_proyectil
   velocidad_ang = v0/r
+  theta = 0
 
-  valores_r = [r]
+  r_valores = [r]
   r_deriv = [v0]
-  valores_theta = [theta]
+  theta_valores = [theta]
   theta_deriv = [velocidad_ang]
 
+  for i in range(2)
+    a_r = aceleracion_asteroide(r_vals[-1])
+    v_r = r_deriv[-1] + a_r * delta_t
+    r_next = r_vals[-1] + v_r * delta_t
+
+    r_valores.append(r_next)
+    r_deriv.append(v_r)
+    theta_next = theta_valores[-1] + theta_deriv[-1] * delta_t
+    theta_valores.append(theta_next)
+    theta_deriv.append(v0 / r_next)
